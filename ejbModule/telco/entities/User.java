db677a2e82ff760ef6c09fab675aafdec2316f3b
Luca <luca3.giudici@mail.polimi.it>
2022-03-27 12:16:13 +0200

    First Commit!

---
+++ ejbModule/telco/entities/User.java
@@ -0,0 +1,77 @@
+package telco.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user", schema = "telco")
+@NamedQuery(name = "User.checkCredentials", query = "SELECT r FROM User r  WHERE r.username = ?1 and r.password = ?2")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String username;
+
+	private String password;
+
+	private String email;
+
+	private Boolean employee;
+
+	private Boolean insolvent;
+
+	public User() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Boolean getInsolvent() {
+		return insolvent;
+	}
+
+	public void setInsolvent(Boolean insolvent) {
+		this.insolvent = insolvent;
+	}
+
+	public Boolean getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Boolean employee) {
+		this.employee = employee;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
